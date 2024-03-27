98706472f58634e8d00def4d797465025d44670f
Braeden Audlin <125329807+25Braeden@users.noreply.github.com>
2024-03-27 06:27:06 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,25 +2,37 @@
 #include <list>
 #include <random>
 using namespace std;
+
 int main() {
-    list <int> myList;
+    list<int> myList;
     random_device rd;
     mt19937 gen(rd());
     uniform_int_distribution<int> dist(1, 1000000);
-    for(int i = 0; i < 100000; i++) {
+
+    for (int i = 0; i < 100000; i++) {
         int randomNumber = dist(gen);
         myList.push_front(randomNumber);
     }
-    int maxValue = myList.front();
-    int minValue = myList.front();
-    for(int num : myList) {
-        if(num > maxValue) {
-            maxValue = num;
+
+    cout << "Enter a number to search for (between 1 - 1,000,000): ";
+    int searchNum; cin >> searchNum;
+
+    int closestNumValue = 1000000;
+    int closestNum;
+
+    for (int num : myList) {
+        int distance = abs(searchNum - num);
+        if (distance < closestNumValue) {
+            closestNumValue = distance;
+            closestNum = num;
         }
-        else if(num < minValue) {
-            minValue = num;
-        }
-        if (num < 10) {cout << num <<endl;} else if (num > 999980) {cout << num <<endl;}
     }
-    cout << "Max value: " << maxValue << " Min value: " << minValue;
+
+    if (closestNumValue == 0) {
+        cout << "Found your number!" << endl;
+    } else {
+        cout << "Did not find your number. The closest number we found to " << searchNum << " was " << closestNum;
+    }
+
+    return 0;
 }
